5e79c6cd62b05497dc74c623d2219ce5f838fd5d
Vivek Bansal <3vivekb@gmail.com>
2015-10-16 23:40:30 -0700

    Initial R Code

---
+++ CitySourced.R
@@ -0,0 +1,60 @@
+#Started by Vivek
+#Oct 16, 2015
+
+CitySourced<- read.delim("~/Github/open311-plan-for-san-jose/2015-10-14-21-49-388.csv",sep="|", skip=0, check.names=FALSE,quote = "",
+           header=T)
+str(CitySourced)
+summary(CitySourced)
+str()
+count(CitySourced$RequestType)
+str(CitySourced)
+
+summary(CitySourced)
+
+CitySourced$Description
+summary(CitySourced$DeviceType)
+summary(CitySourced$HasBeenForwarded)
+summary(CitySourced$InitialBoundaryName)
+summary(CitySourced$Latitude)
+summary(CitySourced$Longitude)
+plot(CitySourced$Longitude)
+
+str(CitySourced$DeviceType)
+count(CitySourced$DeviceModel)
+#write.csv(CitySourced, file = "San_Jose_Citysourced", sep = ',')
+getwd()
+
+str(CitySourced)
+CitySourced$DateCreated<- as.character(CitySourced$DateCreated)
+CitySourced$DateCreated
+Tasks_Merged$End <- as.Date(as.character(Tasks_Merged$ActivityDate), "%Y-%m-%d")
+
+CitySourced$DateCreated<- as.Date(CitySourced$DateCreated, "%m/%d/%Y")
+
+CitySourced$CreatedYear<-year(CitySourced$DateCreated)
+
+str(subset(CitySourced, CitySourced$CreatedYear == "2015"))
+summary(CitySourced$CreatedYear)
+
+count(CitySourced$CreatedYear)
+
+TO15<- subset(CitySourced, CitySourced$CreatedYear == "2015")
+TO14<- subset(CitySourced, CitySourced$CreatedYear == "2014")
+
+summary(TO15$DeviceType)
+summary(TO15$DeviceModel)
+count(TO15$DeviceModel)
+count(TO14$DeviceModel)
+
+count(CitySourced$RequestType, sort = TRUE)
+CitySourced$RequestType
+sort(table(CitySourced$RequestType))
+
+OtherIssues<- subset(CitySourced, CitySourced$RequestType == "Other (Please Describe)")
+InformationOnly<- subset(CitySourced, CitySourced$RequestType == "Information Only (Please Describe)")
+
+str(OtherIssues)
+OtherIssues$Description
+InformationOnly[225,]
+
+OtherIssues[323,]
